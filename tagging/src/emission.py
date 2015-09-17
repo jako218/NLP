b36ebdd03d71eb5bd31b1aff4f48d4893281b60c
jako218 <jack_o_f@hotmail.com>
2015-09-17 17:55:24 +0100

    Update files to use new directory structure

--- tagging/src/emission.py
+++ tagging/src/emission.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python
 
 import sys
+
+sys.path.insert(0, '../utilities')
 import count
 
 def e(x, s):
 	return float(count.bigram_emissions_count(s, x)) / float(count.unigram_emissions_count(s))
 
 if __name__ == "__main__":
-	sys.stderr.write(str(e("the", "at-tl")))
+	sys.stderr.write(str(e("the", "at")))
