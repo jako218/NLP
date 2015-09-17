993890bcadd6c94dc23b62c7da9ce33a091e7f39
jako218 <jack_o_f@hotmail.com>
2015-09-17 20:05:01 +0100

    Add utility for getting all possible tags

---
+++ tagging/utilities/gather_tags.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+def get_tags(counts_file):
+    tags = []
+    l = counts_file.readline()
+    while l:
+        line = l.strip()
+        if line:
+            parts = line.split()
+            if parts[2] not in tags:
+                tags.append(parts[2])
+        l =counts_file.readline()
+    target = open("./tags_dictionary.txt", "w")
+    for tag in tags:
+        target.write(tag + "\n")
