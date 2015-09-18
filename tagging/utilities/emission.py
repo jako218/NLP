2baf224d5ddf31596c294f7e31916b26af50ca2d
jako218 <jack_o_f@hotmail.com>
2015-09-18 23:11:01 +0100

    Remove unused code

--- tagging/utilities/emission.py
+++ tagging/utilities/emission.py
@@ -1,10 +1,6 @@
 #! /usr/bin/python
 
 import sys
-# import count
-
-# sys.path.insert(0, '../src')
-# from simple_tag import counter as counter
 
 def e(x, s, counter):
     # sys.stderr.write("\nID of Counter Object: " + str(id(counter)) + "\n\n")
