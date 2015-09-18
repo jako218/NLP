583e625301dd5ae89d983d64c4ad73589d135ad3
jako218 <jack_o_f@hotmail.com>
2015-09-18 18:52:27 +0100

    Update emissions.py to use new count objects

--- tagging/utilities/emission.py
+++ tagging/utilities/emission.py
@@ -1,7 +1,12 @@
 #! /usr/bin/python
 
 import sys
-import count
+# import count
 
-def e(x, s):
-	return float(count.bigram_emissions_count(s, x)) / float(count.unigram_emissions_count(s))
+# sys.path.insert(0, '../src')
+# from simple_tag import counter as counter
+
+def e(x, s, counter):
+    # sys.stderr.write("\nID of Counter Object: " + str(id(counter)) + "\n\n")
+	
+    return float(counter.wordtag_emissions_count(s, x)) / float(counter.unigram_emissions_count(s))
