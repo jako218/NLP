e1aad0257ebe65e395dd3b94b2becd5a88c985d5
jako218 <jack_o_f@hotmail.com>
2015-09-18 22:40:40 +0100

    Fix create_rare.py to correctly formulate _RARE_ token

--- tagging/utilities/create_rare.py
+++ tagging/utilities/create_rare.py
@@ -3,23 +3,23 @@
 import sys
 
 def create_rare_tag(tags_file, counts_file):
-    rare_words = []
+    rare_words = {}
     c = counts_file.readline()
     while c:
         c_line = c.strip()
         if c_line:
             c_parts = c_line.split()
             if c_parts[1] == "WORDTAG" and int(c_parts[0]) < 5:
-                rare_words.append(c_parts[3])
+                rare_words[c_parts[3]] = c_parts[2]
         c = counts_file.readline()
 
-    target = open("./rare_tags.txt", "w")
+    target = open("../train/rare_tags.txt", "w")
     t = tags_file.readline()
     while t:
         t_line = t.strip()
         if t_line:
             t_parts = t_line.split()
-            if t_parts[0] in rare_words:
+            if t_parts[0] in rare_words and t_parts[1] == rare_words[t_parts[0]]:
                 target.write("_RARE_ " + t_parts[1] + "\n")
             else:
                 target.write(t_line + "\n")
@@ -32,13 +32,13 @@ def create_rare_tag(tags_file, counts_file):
 if __name__ == "__main__":
 
     try:
-        tags_file = file("tagging/train/tags.txt", "r")
+        tags_file = file("../train/tags.txt", "r")
     except IOError:
         sys.stderr.write("ERROR: Cannot read input file %s.\n" % arg)
         sys.exit(1)
 
     try:
-        counts_file = file("tagging/train/tags.counts", "r")
+        counts_file = file("../train/tags.counts", "r")
     except IOError:
         sys.stderr.write("ERROR: Cannot read input file %s.\n" % arg)
         sys.exit(1)
