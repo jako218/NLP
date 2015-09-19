8e0d87ecdad42cd8c0926ab535feadf5ca93306c
jako218 <jack_o_f@hotmail.com>
2015-09-19 09:35:43 +0100

    Add file to contain global object references

---
+++ tagging/true_global.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+# -*- coding: utf-8 -*-
+# Object references that are used across many modules are 
+# stored here.
+
+# counter               - instantiated in src/simple_tag.py
+# tag_dictionary        - instantiated in src/simple_tag.py
