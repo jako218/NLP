b74af5070774fbe61a7e424d0713f708197dab67
jako218 <jack_o_f@hotmail.com>
2015-09-19 09:17:58 +0100

    Remove unused code

--- tagging/utilities/emission.py
+++ tagging/utilities/emission.py
@@ -2,7 +2,8 @@
 
 import sys
 
+sys.path.insert(0, '../src')
+from simple_tag import *
+
 def e(x, s, counter):
-    # sys.stderr.write("\nID of Counter Object: " + str(id(counter)) + "\n\n")
-	
     return float(counter.wordtag_emissions_count(s, x)) / float(counter.unigram_emissions_count(s))
