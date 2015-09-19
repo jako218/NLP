6cf9074df0651183f062c5217030731cec6c112d
jako218 <jack_o_f@hotmail.com>
2015-09-19 10:39:45 +0100

    Handle edge cases ('*' and 'STOP' tags)

--- tagging/utilities/transition.py
+++ tagging/utilities/transition.py
@@ -1,3 +1,16 @@
 #! /usr/bin/python
 
 import sys
+
+sys.path.insert(0, '../')
+import true_global as g
+
+def q(s1, s3, s2):
+    if s3 == "*" and s2 == "*":
+        return float(g.counter.unigram_count(s1)) / float(g.counter.tag_count())
+    elif s3 == "*":
+        return float(g.counter.bigram_count(s2, s1)) / float(g.counter.unigram_count(s2))
+    elif s1 == "STOP":
+        return float(g.counter.bigram_count(s3, s2))
+    else:
+        return float(g.counter.trigram_count(s3, s2, s1)) / float(g.counter.bigram_count(s3, s2))
