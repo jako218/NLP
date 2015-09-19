f65a1b4c0db6263c1dfd1864715913f7a71ad899
jako218 <jack_o_f@hotmail.com>
2015-09-19 09:31:05 +0100

    Update emission to use global file for global class instances used across modules

--- tagging/utilities/emission.py
+++ tagging/utilities/emission.py
@@ -2,8 +2,8 @@
 
 import sys
 
-sys.path.insert(0, '../src')
-from simple_tag import *
+sys.path.insert(0, '../')
+import true_global as g
 
-def e(x, s, counter):
-    return float(counter.wordtag_emissions_count(s, x)) / float(counter.unigram_emissions_count(s))
+def e(x, s):
+    return float(g.counter.wordtag_emissions_count(s, x)) / float(g.counter.unigram_emissions_count(s))
