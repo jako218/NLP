ecbe4bc6d2b0524df9634e36a60a78124187d471
jako218 <jack_o_f@hotmail.com>
2015-09-19 10:40:56 +0100

    Edit function names to be more appropriate

--- tagging/utilities/emission.py
+++ tagging/utilities/emission.py
@@ -6,4 +6,4 @@ sys.path.insert(0, '../')
 import true_global as g
 
 def e(x, s):
-    return float(g.counter.wordtag_emissions_count(s, x)) / float(g.counter.unigram_emissions_count(s))
+    return float(g.counter.wordtag_count(s, x)) / float(g.counter.unigram_count(s))
